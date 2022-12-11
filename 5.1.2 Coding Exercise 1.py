77d425ec9050f3b5ce6c2c58bc20b5fe0164b169
salioucd <111522223+salioucd@users.noreply.github.com>
2022-12-11 17:53:26 -0600

    Create 5.1.2 Coding Exercise 1.py

---
+++ 5.1.2 Coding Exercise 1.py
@@ -0,0 +1,30 @@
+#Define the class Person
+class Person:
+    #Create a new instance of Person
+    def __init__(self):
+        #Person's default values
+        self.firstname = "[no first name]"
+        self.lastname = "[no last name]"
+        self.eyecolor = "[no eye color]"
+        self.age = -1
+
+        
+ #Exercise 2
+
+#Define the class Name
+class Name:
+    def __init__(self):
+        self.firstname = "[no first name]"
+        self.lastname = "[no last name]"
+
+#Define the class Person
+class Person:
+    #Create a new instance of Person
+    def __init__(self):
+        #Person's default values
+        self.name = Name()
+        self.eyecolor = "[no eye color]"
+        self.age = -1
+
+        
+        
